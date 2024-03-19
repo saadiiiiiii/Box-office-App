8c0163f140110a07305004bc6d20b0afc7327eda
saad <saadahmad01799@gmail.comm>
2024-03-19 21:09:07 +0500

    search for movies on click

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,24 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Home = () => {
+  const [searchStr, setSearchStr] = useState('');
+  const onInputChange = e => {
+    console.log(e.target.value);
+    setSearchStr(e.target.value);
+  };
+  const onSearch = async e => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
   return (
     <div>
-      <Link to="/Starred">Go to starred page</Link>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onInputChange} />
+        <button type="submit">Cleek meee</button>
+      </form>
     </div>
   );
 };
